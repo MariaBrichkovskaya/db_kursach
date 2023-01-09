85963db6146494757896c7d1ee6a6536dd202481
DmitryPasmurtsev <dima427614@gmail.com>
2023-01-10 00:10:51 +0300

    добавление заказа в процессе

--- kursach/src/main/java/com/db/kursach/controllers/OrderController.java
+++ kursach/src/main/java/com/db/kursach/controllers/OrderController.java
@@ -3,7 +3,9 @@ package com.db.kursach.controllers;
 
 
 import com.db.kursach.models.Order;
+import com.db.kursach.services.EmployeeService;
 import com.db.kursach.services.OrderService;
+import com.db.kursach.services.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +18,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class OrderController {
 
     private final OrderService orderService;
+    private final ProductService productService;
+    private final EmployeeService employeeService;
+
+    Order order;
 
     @GetMapping("/orders")
     public String orders(Model model){
@@ -28,9 +34,28 @@ public class OrderController {
         model.addAttribute("order",order);
         return "order-info";
     }
+
+
+    @PostMapping("/order/addNew")
+    public String createOrder(Model model){
+        order = new Order();
+        model.addAttribute("order", new Order());
+        model.addAttribute("products", productService.listProducts());
+        model.addAttribute("employees", employeeService.listEmployees(null));
+        return "order-creation";
+    }
+
     @PostMapping("/order/create")
-    public String createOrder(Order order){
-        orderService.saveOrder(order);
-        return "redirect:/";
+    public String editOrder(Model model){
+        model.addAttribute("order", new Order());
+        model.addAttribute("products", productService.listProducts());
+        model.addAttribute("employees", employeeService.listEmployees(null));
+        return "order-creation";
+    }
+
+    @PostMapping("/order/create/addProduct")
+    public String addProductInOrder(Order order){
+        //order.
+        return "redirect:/order/create";
     }
 }
